8b09064e9d621a313e620bc40a333ba68cc4b6bd
jihoseo2006 <jihoseo2006@gmail.com>
2022-09-15 22:38:52 +0900

    2908
    2908번 정답

--- algo/algo/소스.c
+++ algo/algo/소스.c
@@ -7,10 +7,33 @@
 //	return 0;
 //}
 
-#include<stdio.h>//문제 1001번
+//#include<stdio.h>//문제 1001번
+//int main() {
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d", a - b);
+//	return 0;
+//}
+
+#include<stdio.h>//2908번
 int main() {
 	int a, b;
+	int a1, a2, a3,a4, b1, b2, b3,b4;
 	scanf("%d %d", &a, &b);
-	printf("%d", a - b);
+	a1 = a / 100;
+	a2 = a / 10 % 10;
+	a3 = a % 100 %10;
+	b1 = b / 100;
+	b2 = b / 10 % 10;
+	b3 = b % 100 % 10;
+	a4 = (a3 * 100) + (a2 * 10) + a1;
+	b4 = (b3 * 100) + (b2 * 10) + b1;
+	if (a4 > b4) {
+		printf("%d", a4);
+	}
+	else
+	{
+		printf("%d", b4);
+	}
 	return 0;
 }
