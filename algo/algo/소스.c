57e53cfc8da627abfa9755f40e733516ed8199c3
jihoseo2006 <jihoseo2006@gmail.com>
2022-09-15 15:47:23 +0900

    알고리즘 풀이용 소스
    알고리즘용 소스코드 커밋

---
+++ algo/algo/소스.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>//문제 1000번
+//int main() {
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d", a + b);
+//	return 0;
+//}
+
+#include<stdio.h>//문제 1001번
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a - b);
+	return 0;
+}
