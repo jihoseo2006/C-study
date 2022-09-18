bda511f8fb5be400b161976f77a8be3dd9a2ccc1
jihoseo2006 <jihoseo2006@gmail.com>
2022-09-18 23:14:37 +0900

    2439
    아직 제작중

--- algo/algo/소스.c
+++ algo/algo/소스.c
@@ -230,15 +230,31 @@
 //	return 0;
 //}
 
-#include<stdio.h>//2438번
+//#include<stdio.h>//2438번
+//int main() {
+//	int a;
+//	scanf("%d", &a);
+//	for (int i = 1; i <= a; i++) {
+//		for (int j = 0; j < i; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+
+#include<stdio.h>//2439번 미완성
 int main() {
-	int a;
-	scanf("%d", &a);
-	for (int i = 1; i <= a; i++) {
-		for (int j = 0; j < i; j++)
-		{
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i <= n; i++) {
+		for (int j = n-1; j > 0; j--) {
+			printf(" ");
+		}
+		for (int k = 0; k <= n; k++) {
 			printf("*");
 		}
 		printf("\n");
 	}
+	return 0;
 }
