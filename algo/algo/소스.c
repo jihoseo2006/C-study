4e8b8cb08022002771d498d757dd5b8a5f6b8b26
jihoseo2006 <jihoseo2006@gmail.com>
2022-09-19 09:28:06 +0900

    2439미완성
    미완성.

--- algo/algo/소스.c
+++ algo/algo/소스.c
@@ -243,18 +243,19 @@
 //	}
 //}
 
-#include<stdio.h>//2439번 미완성
-int main() {
-	int n;
-	scanf("%d", &n);
-	for (int i = 0; i <= n; i++) {
-		for (int j = n-1; j > 0; j--) {
-			printf(" ");
-		}
-		for (int k = 0; k <= n; k++) {
-			printf("*");
-		}
-		printf("\n");
-	}
-	return 0;
-}
+//#include<stdio.h>//2439번 미완성
+//int main() {
+//	int n;
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++) {
+//		for (int j = n-1; j > 0; j--) {
+//			printf(" ");
+//		}
+//		for (int k = 0; k <= i; k++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
