6d795004308188c271c898cd0c9f09d324f47b1c
jihoseo2006 <jihoseo2006@gmail.com>
2022-09-28 19:56:02 +0900

    주석처리

--- Re_Fucking_Start/Re_Fucking_Start/Re_기초.c
+++ Re_Fucking_Start/Re_Fucking_Start/Re_기초.c
@@ -466,50 +466,51 @@
 //개선된 스택 프로그램
 //구조체 수택은 머리가 어지려워요..
 
-#include<stdio.h>
-#define MAX_STACK_SIZE 100
-struct StackType {
-	int data[MAX_STACK_SIZE]; //스택을 구조체 멤버로 넣음으로써 한 프로그램에서 여러 스택 만들 수 있음
-	int top;
-};
+//#include<stdio.h>
+//#define MAX_STACK_SIZE 100
+//struct StackType {
+//	int data[MAX_STACK_SIZE]; //스택을 구조체 멤버로 넣음으로써 한 프로그램에서 여러 스택 만들 수 있음
+//	int top;
+//};
+//
+//// 스택 초기화 함수
+//void init_stack(struct StackType* s)
+//{
+//	s->top = -1;
+//}
+//
+////구조체 StackType의 멤버 top을 활용하여 공백상태 확인
+//int is_empty(struct StackType* s)
+//{
+//	return s->top == -1;
+//}
+////구조체 StackType의 멤버 top을 활용하여 포화상태 확인
+//int is_full(struct StackType* s)
+//{
+//	return s->top == MAX_STACK_SIZE - 1;
+//}
+//// 삽입함수
+//void push(struct StackType* s, int item)
+//{
+//	if (is_full(s)) printf("Push error!");
+//	else s->data[++s->top] = item;
+//}
+//// 삭제함수
+//int pop(struct StackType* s)
+//{
+//	if (is_empty(s)) printf("Pop error!");
+//	else return s->data[s->top--];
+//}
+//int main(void)
+//{
+//	struct StackType s;  // 스택을 정적으로 생성
+//
+//	init_stack(&s);   // 함수를 호출할 때 매개변수로 스택의 주소를 전달
+//	push(&s, 1);
+//	push(&s, 2);
+//	push(&s, 3);
+//	printf("%d\n", pop(&s));
+//	printf("%d\n", pop(&s));
+//	printf("%d\n", pop(&s));
+//}
 
-// 스택 초기화 함수
-void init_stack(struct StackType* s)
-{
-	s->top = -1;
-}
-
-//구조체 StackType의 멤버 top을 활용하여 공백상태 확인
-int is_empty(struct StackType* s)
-{
-	return s->top == -1;
-}
-//구조체 StackType의 멤버 top을 활용하여 포화상태 확인
-int is_full(struct StackType* s)
-{
-	return s->top == MAX_STACK_SIZE - 1;
-}
-// 삽입함수
-void push(struct StackType* s, int item)
-{
-	if (is_full(s)) printf("Push error!");
-	else s->data[++s->top] = item;
-}
-// 삭제함수
-int pop(struct StackType* s)
-{
-	if (is_empty(s)) printf("Pop error!");
-	else return s->data[s->top--];
-}
-int main(void)
-{
-	struct StackType s;  // 스택을 정적으로 생성
-
-	init_stack(&s);   // 함수를 호출할 때 매개변수로 스택의 주소를 전달
-	push(&s, 1);
-	push(&s, 2);
-	push(&s, 3);
-	printf("%d\n", pop(&s));
-	printf("%d\n", pop(&s));
-	printf("%d\n", pop(&s));
-}
